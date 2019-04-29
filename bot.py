60e48fd6c83a36f3be002968a36cd459180d2293
LCat <ambronya@gmail.com>
2019-04-29 18:00:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 # Настройки
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import apiai, json
-updater = Updater(token='788393244:AAG-LX1wS8qjGKmFZePN7bgQ9qz-9xlPeqU') # Токен API к Telegram
+updater = Updater(token='-----------------------') # Токен API к Telegram
 dispatcher = updater.dispatcher
 # Обработка команд
 def startCommand(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
 def textMessage(bot, update):
-    request = apiai.ApiAI('23887f143e884143afb2004a0da4b420').text_request() # Токен API к Dialogflow
+    request = apiai.ApiAI('------------------').text_request() # Токен API к Dialogflow
     request.lang = 'ru' # На каком языке будет послан запрос
     request.session_id = 'aidos_project_bot' # ID Сессии диалога (нужно, чтобы потом учить бота)
     request.query = update.message.text # Посылаем запрос к ИИ с сообщением от юзера
